5a70c9ea8ba9ac173308584dfe7bb3a803019f97
AroobMushtaq <aroobmushtaq786@gmail.com>
2025-10-14 23:14:18 +0500

    llm

---
+++ 05_llm/01_llm_basic/02_gemini_strem.py
@@ -0,0 +1,15 @@
+from langchain_google_genai import GoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+
+llm = GoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+    )
+
+question = input("Enter you prompt: ")
+
+
+for chunk in llm.stream(question):
+    print(chunk)
